46c15465a66d33072798a23678b351513e9dcf1c
KopiSusu45 <113882841+KopiSusu45@users.noreply.github.com>
2024-05-27 20:24:34 +0700

    home.php

--- home.php
+++ home.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
 include 'koneksi.php';
-$userid =$_SESSION['username'];
+$id =$_SESSION['username'];
 if (!isset($_SESSION['status'])) {
     $_SESSION['status'] = 'not_login';
 }
@@ -60,8 +60,8 @@ if ($_SESSION['status'] != 'login') {
                     <a class='nav-link' href='author.php'>Author</a>
                 </li>
                 <?php
-                $userid = $_SESSION['userid'];
-                $sql = mysqli_query($conn, "SELECT * FROM user WHERE userid='$userid'");
+                $id = $_SESSION['id'];
+                $sql = mysqli_query($conn, "SELECT * FROM absen WHERE id='$id'");
                 while ($data = mysqli_fetch_array($sql)) {
                     ?>
                 <?php
@@ -70,7 +70,7 @@ if ($_SESSION['status'] != 'login') {
                     echo "<li class='nav-item dropdown'>
                 <a class='nav-link dropdown-toggle' href='author' id='navbarDropdownMenuLink' 
                 role='button' data-toggle='dropdown' aria-haspopup='true' aria-expanded='false'>
-                <img class='rounded-circle mr-2' src='assets/img/" . $data['namafoto_pro'] . "' width='30'>
+                
                 <span class='align-middle'>$username</span></a>
                 <div class='dropdown-menu' aria-labelledby='navbarDropdownMenuLink'>
                 <a class='dropdown-item' href='album.php'>Album</a>
@@ -92,117 +92,7 @@ if ($_SESSION['status'] != 'login') {
                 <?php } ?>
             </ul>
         </div>
-    </nav>
-
-    <section class="mt-4 mb-5">
-        <div class="container-fluid mb-4">
-            <h1 class="font-weight-bold title">Jelajahi Foto</h1>
-            <div class="row">
-                <nav class="navbar navbar-expand-lg navbar-light bg-white pl-2 pr-2">
-                    <button class="navbar-light navbar-toggler" type="button" data-toggle="collapse"
-                        data-target="#navbarsExplore" aria-controls="navbarsDefault" aria-expanded="false"
-                        aria-label="Toggle navigation">
-                        <span class="navbar-toggler-icon"></span>
-                    </button>
-                    <?php
-                    $kategori = mysqli_query($conn, "SELECT * FROM kategori");
-                    echo "<a href='home.php' class='btn btn-outline-secondary btn-sm'>All </a>";
-                    while ($row = mysqli_fetch_array($kategori)) { ?>
-
-                        <div class="collapse navbar-collapse" id="navbarsExplore">
-                            <ul class="navbar-nav">
-                                <li class="nav-item">
-                                    <a href="home.php?kategoriid=<?php echo $row['kategoriid'] ?>" class="nav-link">
-                                        <?php echo $row['nama_kategori'] ?>
-                                    </a>
-                            </ul>
-                        </div>
-                    <?php } ?>
-                </nav>
-            </div>
-        </div>
-        <div class="container-fluid mt-3">
-        <div class="row">
-        <?php
-        if(isset($_GET['kategoriid'])){
-            $kategoriid =$_GET['kategoriid'];
-            $query =mysqli_query($conn, "SELECT * FROM foto WHERE userid='$userid' AND kategoriid='$kategoriid'");
-            if (mysqli_num_rows($query) > 0) {?>
-            <?php while($data = mysqli_fetch_assoc($query)){?>
-                <div class="col-md-3 mt-2">
-                <div class="card card-pin"  style="width: 18rem;">
-                    <img style="width: 100%; height: 18rem; object-fit: cover;" 
-                    class="card-img-top" src="assets/img/<?php echo $data['lokasifile']?>" alt="Card image">
-                    <div class="overlay" style="width: 100%; height: 18rem; object-fit: cover;">
-                        <h2 class="card-title title"><?php echo $data['judulfoto'] ?></h2>
-                        <div class="more">
-                            <a href="detail.php?fotoid=<?php echo $data['fotoid'] ?>">
-                            <i class="fa fa-arrow-circle-o-right" aria-hidden="true"></i> Selengkapnya </a>
-                        </div>
-                    </div>
-                    <div class="card-footer">
-                        <h5 class="card-text text-center"><?php echo $data['judulfoto'] ?></h5>
-                        <p class="card-text"><?php echo $data['userid'] ?></p>
-                        <p class="card-text"><?php echo $data['tanggalunggah'] ?></p>
-                            <?php
-                                $fotoid = $data['fotoid'];
-                                $ceksuka =mysqli_query($conn, "SELECT * FROM likefoto WHERE fotoid='$fotoid' AND userid='$userid'");
-                                    if(mysqli_num_rows($ceksuka)== 1){?>
-                                        <a href="proses_like.php?fotoid=<?php echo $data['fotoid']?>" type="submit" name="batalsuka">
-                                        <i class="fa fa-heart"></i></a>
-                                    <?php }else{?>
-                                        <a href="proses_like.php?fotoid=<?php echo $data['fotoid']?>" type="submit" name="suka">
-                                        <i class="fa-regular fa-heart"></i></a>
-                                    <?php }
-                                $like = mysqli_query($conn, "SELECT * FROM likefoto WHERE fotoid='$fotoid'");
-                                echo mysqli_num_rows($like).' Suka';
-                            ?>
-                        </a>
-                        <a href=""><i class="fa-regular fa-comment"></i>3 komentar</a>
-                    </div>
-                </div>
-              </div>
-    <?php   }}
-        }else{   
-        $query = mysqli_query($conn, "SELECT * FROM foto");
-        while ($data = mysqli_fetch_array($query)) {?>
-        <div class="col-md-3 mt-2">
-                <div class="card card-pin"  style="width: 18rem;">
-                    <img style="width: 100%; height: 18rem; object-fit: cover;" class="card-img-top" 
-                    src="assets/img/<?php echo $data['lokasifile']?>" alt="Card image">
-                    <div class="overlay" style="width: 100%; height: 18rem; object-fit: cover;">
-                        <h2 class="card-title title"><?php echo $data['judulfoto'] ?></h2>
-                        <div class="more">
-                            <a href="detail.php?fotoid=<?php echo $data['fotoid'] ?>">
-                            <i class="fa fa-arrow-circle-o-right" aria-hidden="true"></i> Selengkapnya </a>
-                        </div>
-                    </div>
-                    <div class="card-footer">
-                        <h5 class="card-text text-center"><?php echo $data['judulfoto'] ?></h5>
-                        <p class="card-text"><?php echo $data['userid'] ?></p>
-                        <p class="card-text"><?php echo $data['tanggalunggah'] ?></p>
-                            <?php
-                                $fotoid = $data['fotoid'];
-                                $ceksuka =mysqli_query($conn, "SELECT * FROM likefoto WHERE fotoid='$fotoid' AND userid='$userid'");
-                                    if(mysqli_num_rows($ceksuka)== 1){?>
-                                        <a href="proses_like.php?fotoid=<?php echo $data['fotoid']?>" type="submit" name="batalsuka">
-                                        <i class="fa fa-heart"></i></a>
-                                    <?php }else{?>
-                                        <a href="proses_like.php?fotoid=<?php echo $data['fotoid']?>" type="submit" name="suka">
-                                        <i class="fa-regular fa-heart"></i></a>
-                                    <?php }
-                                $like = mysqli_query($conn, "SELECT * FROM likefoto WHERE fotoid='$fotoid'");
-                                echo mysqli_num_rows($like).' Suka';
-                            ?>
-                        </a>
-                        <a href=""><i class="fa-regular fa-comment"></i>3 komentar</a>
-                    </div>
-                  </div>
-                 </div>
-     <?php } } ?>
-    </div>
-    </div>
-    </section>
+</nav>
     <script src="assets/js/app.js"></script>
     <script src="assets/js/theme.js"></script>
     <script src="assets/js/bootstrap.min.js"></script>
